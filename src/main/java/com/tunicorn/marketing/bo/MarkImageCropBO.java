f0dc52294b93c062665b068fc73887bdafd2b45b
lining <lining@tunicorn.cn>
2017-09-12 17:52:05 +0800

    optimized code

---
+++ src/main/java/com/tunicorn/marketing/bo/MarkImageCropBO.java
@@ -0,0 +1,61 @@
+package com.tunicorn.marketing.bo;
+
+import com.fasterxml.jackson.databind.node.ArrayNode;
+
+public class MarkImageCropBO {
+	private int order;
+	private ArrayNode imageCrop;
+	private String imageId;
+	private String majorType;
+	private String filePath;
+	private String imagePath;
+
+	public int getOrder() {
+		return order;
+	}
+
+	public void setOrder(int order) {
+		this.order = order;
+	}
+
+	public ArrayNode getImageCrop() {
+		return imageCrop;
+	}
+
+	public void setImageCrop(ArrayNode imageCrop) {
+		this.imageCrop = imageCrop;
+	}
+
+	public String getImageId() {
+		return imageId;
+	}
+
+	public void setImageId(String imageId) {
+		this.imageId = imageId;
+	}
+
+	public String getMajorType() {
+		return majorType;
+	}
+
+	public void setMajorType(String majorType) {
+		this.majorType = majorType;
+	}
+
+	public String getFilePath() {
+		return filePath;
+	}
+
+	public void setFilePath(String filePath) {
+		this.filePath = filePath;
+	}
+
+	public String getImagePath() {
+		return imagePath;
+	}
+
+	public void setImagePath(String imagePath) {
+		this.imagePath = imagePath;
+	}
+
+}
--- src/main/java/com/tunicorn/marketing/service/ErrorCorrectionDetailService.java
+++ src/main/java/com/tunicorn/marketing/service/ErrorCorrectionDetailService.java
@@ -1,11 +1,24 @@
 package com.tunicorn.marketing.service;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
+import javax.imageio.ImageIO;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.RandomStringUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
+import org.dom4j.Document;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
+import org.dom4j.io.OutputFormat;
+import org.dom4j.io.XMLWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,8 +26,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.tunicorn.marketing.bo.CropBO;
+import com.tunicorn.marketing.bo.GoodsSkuBO;
+import com.tunicorn.marketing.bo.MarkImageCropBO;
+import com.tunicorn.marketing.constant.MarketingConstants;
 import com.tunicorn.marketing.mapper.ErrorCorrectionDetailMapper;
+import com.tunicorn.marketing.mapper.GoodsSkuMapper;
+import com.tunicorn.marketing.mapper.TaskImagesMapper;
+import com.tunicorn.marketing.utils.ConfigUtils;
 import com.tunicorn.marketing.vo.ErrorCorrectionDetailVO;
+import com.tunicorn.marketing.vo.GoodsSkuVO;
+import com.tunicorn.marketing.vo.TaskImagesVO;
 
 @Service
 public class ErrorCorrectionDetailService {
@@ -22,6 +43,10 @@ public class ErrorCorrectionDetailService {
 
 	@Autowired
 	private ErrorCorrectionDetailMapper errorCorrectionDetailMapper;
+	@Autowired
+	private GoodsSkuMapper goodsSkuMapper;
+	@Autowired
+	private TaskImagesMapper taskImagesMapper;
 
 	public int createErrorCorrectionDetail(ErrorCorrectionDetailVO errorCorrectionDetailVO) {
 		return errorCorrectionDetailMapper.createErrorCorrectionDetail(errorCorrectionDetailVO);
@@ -37,6 +62,30 @@ public class ErrorCorrectionDetailService {
 
 	public ErrorCorrectionDetailVO getErrorCorrectionDetailByImageId(String imageId) {
 		return errorCorrectionDetailMapper.getErrorCorrectionDetailByImageId(imageId);
+	}
+
+	public void saveMarkImageCrop(MarkImageCropBO markImageCropBO) {
+		String imageId = markImageCropBO.getImageId();
+		if (StringUtils.isNotBlank(imageId)) {
+			ErrorCorrectionDetailVO correctionDetailVO = errorCorrectionDetailMapper
+					.getErrorCorrectionDetailByImageId(imageId);
+			if (correctionDetailVO != null) {
+				correctionDetailVO.setResult(markImageCropBO.getImageCrop().toString());
+				this.updateErrorCorrectionDetail(correctionDetailVO);
+			} else {
+				correctionDetailVO = new ErrorCorrectionDetailVO();
+				correctionDetailVO.setId(
+						(Long.toHexString(new Date().getTime()) + RandomStringUtils.randomAlphanumeric(13)).toLowerCase());
+				correctionDetailVO.setResult(markImageCropBO.getImageCrop().toString());
+				correctionDetailVO.setImageId(markImageCropBO.getImageId());
+				correctionDetailVO.setFilePath(markImageCropBO.getFilePath());
+				correctionDetailVO.setImageId(markImageCropBO.getImageId());
+				correctionDetailVO.setImagePath(markImageCropBO.getImagePath());
+				correctionDetailVO.setMajorType(markImageCropBO.getMajorType());
+				this.createErrorCorrectionDetail(correctionDetailVO);
+			}
+			generateFile(markImageCropBO);
+		}
 	}
 
 	public List<CropBO> getTaskMarkImageCrops(String imageId, Integer imageOrderNo) {
@@ -65,10 +114,120 @@ public class ErrorCorrectionDetailService {
 					}
 				}
 			} catch (IOException e) {
-				logger.error("imageId:" + imageId + ", getTaskMarkImageCrops method 获取getTaskMarkImageCrops失败, " + e.getMessage());
+				logger.error("imageId:" + imageId + ", getTaskMarkImageCrops method 获取getTaskMarkImageCrops失败, "
+						+ e.getMessage());
 			}
 		}
 
 		return cropBOs;
+	}
+
+	private void generateFile(MarkImageCropBO cropBO) {
+		String xmlFilePath = String.format("%s%s%s%s%s%s%s%s",
+				com.tunicorn.util.ConfigUtils.getInstance().getConfigValue("storage.private.basePath"),
+				ConfigUtils.getInstance().getConfigValue("marketing.image.root.path"), File.separator,
+				cropBO.getMajorType(), File.separator, MarketingConstants.CROP_TXT_PATH, File.separator,
+				cropBO.getImageId() + ".xml");
+		File imageFile;
+		TaskImagesVO imagesVO = taskImagesMapper.getTaskImagesById(cropBO.getImageId());
+		String imageFilenameTemp = "";
+		if (imagesVO != null && imagesVO.getFullPath() != null) {
+			try {
+				imageFile = new File(imagesVO.getFullPath());
+				imageFilenameTemp = String.format("%s%s%s%s%s%s%s%s",
+						com.tunicorn.util.ConfigUtils.getInstance().getConfigValue("storage.private.basePath"),
+						ConfigUtils.getInstance().getConfigValue("marketing.image.root.path"), File.separator,
+						cropBO.getMajorType(), File.separator, MarketingConstants.CROP_IMAGE_PATH, File.separator,
+						cropBO.getImageId() + ".jpg");
+				FileUtils.copyFile(new File(imagesVO.getFullPath()), new File(imageFilenameTemp));
+
+				BufferedImage bufferedImage = ImageIO.read(imageFile);
+				int width = bufferedImage.getWidth();
+				int height = bufferedImage.getHeight();
+				generateXmlFile(cropBO, xmlFilePath, width, height);
+			} catch (IOException e) {
+				logger.error("imageId:" + cropBO.getImageId() + ", copy file fail, " + e.getMessage());
+			}
+		}
+
+		ErrorCorrectionDetailVO errorCorrectionDetailVO = new ErrorCorrectionDetailVO();
+		errorCorrectionDetailVO.setId(
+				(Long.toHexString(new Date().getTime()) + RandomStringUtils.randomAlphanumeric(13)).toLowerCase());
+		errorCorrectionDetailVO.setMajorType(cropBO.getMajorType());
+		errorCorrectionDetailVO.setImagePath(imageFilenameTemp);
+		errorCorrectionDetailVO.setFilePath(xmlFilePath);
+		errorCorrectionDetailMapper.createErrorCorrectionDetail(errorCorrectionDetailVO);
+	}
+
+	private void generateXmlFile(MarkImageCropBO cropBO, String xmlFilePath, int width, int height) {
+		Element root = DocumentHelper.createElement("annotation");
+		Document document = DocumentHelper.createDocument(root);
+
+		// 给根节点添加孩子节点
+		root.addElement("foder").addText("newdata");
+		root.addElement("filename").addText(cropBO.getImageId());
+		root.addElement("path").addText(xmlFilePath);
+
+		Element sourceElement = root.addElement("source");
+		sourceElement.addElement("database").addText("mysql");
+
+		Element sizeElement = root.addElement("size");
+		sizeElement.addElement("width").addText(String.valueOf(width));
+		sizeElement.addElement("height").addText(String.valueOf(height));
+		sizeElement.addElement("depth").addText("3");
+
+		root.addElement("segmented").addText("0");
+		if (cropBO != null && cropBO.getImageCrop() != null && cropBO.getImageCrop().size() > 0) {
+			ArrayNode arrayNode = cropBO.getImageCrop();
+			for (int j = 0; j < arrayNode.size(); j++) {
+				Element objectElement = root.addElement("object");
+
+				ObjectNode nodeResult = (ObjectNode) arrayNode.get(j);
+				String labelName = "Others";
+				GoodsSkuBO goodsSkuBO = new GoodsSkuBO();
+				goodsSkuBO.setOrder(nodeResult.get("label").asInt() - 1);
+				goodsSkuBO.setMajorType(cropBO.getMajorType());
+				List<GoodsSkuVO> goodsSkuVOs = goodsSkuMapper.getGoodsSkuListByBO(goodsSkuBO);
+				if (goodsSkuVOs != null && goodsSkuVOs.size() > 0) {
+					labelName = goodsSkuVOs.get(0).getName();
+				}
+				objectElement.addElement("name").addText(labelName);
+				objectElement.addElement("pose").addText("Unspecified");
+				objectElement.addElement("truncated").addText("0");
+				objectElement.addElement("difficult").addText("0");
+				Element bndboxElement = objectElement.addElement("bndbox");
+				int x = 0;
+				int y = 0;
+				if (nodeResult.get("x") != null) {
+					bndboxElement.addElement("xmin").addText(nodeResult.get("x").toString());
+					x = nodeResult.get("x").asInt();
+				}
+				if (nodeResult.get("y") != null) {
+					bndboxElement.addElement("ymin").addText(nodeResult.get("y").toString());
+					y = nodeResult.get("y").asInt();
+				}
+				if (nodeResult.get("width") != null) {
+					bndboxElement.addElement("xmax").addText(String.valueOf(x + nodeResult.get("width").asInt()));
+				}
+				if (nodeResult.get("height") != null) {
+					bndboxElement.addElement("ymax").addText(String.valueOf(y + nodeResult.get("height").asInt()));
+				}
+			}
+		}
+		try {
+			OutputFormat format = OutputFormat.createPrettyPrint();
+			format.setEncoding("UTF-8");
+			// 设置声明之后不换行
+			format.setNewLineAfterDeclaration(false);
+			File file = new File(xmlFilePath);
+			file.setWritable(true, false);
+			FileUtils.writeStringToFile(file, StringUtils.EMPTY);
+			XMLWriter xmlWriter = new XMLWriter(new FileOutputStream(file), format);
+			xmlWriter.write(document);
+			xmlWriter.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 }

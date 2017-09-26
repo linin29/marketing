fbc47f08a8787b6ff37ee68b4f939ee3ce2ee299
lining <lining@tunicorn.cn>
2017-09-26 17:25:30 +0800

    optimized code

--- src/main/java/com/tunicorn/marketing/utils/FTPClientHelper.java
+++ src/main/java/com/tunicorn/marketing/utils/FTPClientHelper.java
@@ -30,7 +30,7 @@ public class FTPClientHelper {
 												   com.tunicorn.util.ConfigUtils.getInstance().getConfigValue("storage.private.basePath"),
 												   ConfigUtils.getInstance().getConfigValue("marketing.image.root.path"), File.separator,
 												   MarketingConstants.UPLOAD_PATH);
-	private final static String SITE_UMASK_COMMAND = "site umask 666";
+	private final static String SITE_UMASK_COMMAND = "site umask 022";
 	private FTPClient ftpClient = new FTPClient();     
     private String server, userName, password;
     private int port;

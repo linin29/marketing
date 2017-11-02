82e6011f0608ed841b8ab08e215461fb157e0492
lining <lining@tunicorn.cn>
2017-11-02 15:12:45 +0800

    fix bug

--- src/main/resources/static/js/aec.js
+++ src/main/resources/static/js/aec.js
@@ -13,6 +13,39 @@ aec = (function(){
 			checkedIds = [];
 			queryTask();
 		});
+	 	$('#zip_import').click(function(){
+	 		 $('#file_select').click();
+	 	});
+	 	$('#file_select').change(function(){
+	 		 var _file = $(this)[0];
+	 		 var files = _file.files;
+	 		 var total = files.length;
+	 		 var url = m_url + '/aec/upload';
+
+	 		 var formData = new FormData();
+	 		 
+	 		 for (var i = 0; i < files.length; i++) {
+	 		      var file = files[i];
+	 		      formData.append('zipFile', file, file.name);
+	 		 }
+	 		 $('#waiting').modal({keyboard: false, backdrop: 'static'});
+	 		 $('#waiting').modal('show');
+	 		 $.ajax({
+	 			type: "POST",
+	 			url: url,
+	 			data : formData, 
+	 			processData : false, 
+	 			contentType : false,
+	 			success: function (resp) {
+	 				 $('#waiting').modal('hide');
+	 				 noty({text: "纠错成功", layout: "topCenter", type: "success", timeout: 1000});
+	 			},
+	 			error: function (message) {
+	 				 $('#waiting').modal('hide');
+	 				 noty({text: "纠错失败", layout: "topCenter", type: "error", timeout: 1000});
+	 			}
+	 		 });
+	 	});
 		$("#download").click(function(){
 			var data = {taskIds:checkedIds};
 			$.ajax({

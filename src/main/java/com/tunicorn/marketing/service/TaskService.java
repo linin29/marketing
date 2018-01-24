e119727f845d06d0433bb69fc303bc58ef57e154
lining <lining@tunicorn.cn>
2018-01-24 13:56:12 +0800

    添加获取拼接图片接口

--- src/main/java/com/tunicorn/marketing/service/TaskService.java
+++ src/main/java/com/tunicorn/marketing/service/TaskService.java
@@ -447,9 +447,10 @@ public class TaskService {
 			}
 		}
 	}
-	
+
 	/**
 	 * 同步拼接图片业务
+	 * 
 	 * @auther weixiaokai
 	 * @date 2018年1月18日 下午9:00:53
 	 * @param taskId
@@ -1278,6 +1279,19 @@ public class TaskService {
 
 	public List<GoodsSkuVO> getGoodsSkuListByMajorTypeWithShow(String majorType) {
 		return goodsSkuMapper.getGoodsSkuListByMajorTypeWithShow(majorType);
+	}
+
+	public ServiceResponseBO getStitchImageByTaskId(String taskId) {
+		TaskVO taskVO = taskMapper.getTaskById(taskId);
+		if (taskVO == null) {
+			return new ServiceResponseBO(false, "marketing_task_not_existed");
+		}
+		String stitchImage = StringUtils.EMPTY;
+		if (StringUtils.isNotBlank(taskVO.getStitchImagePath())) {
+			stitchImage = ConfigUtils.getInstance().getConfigValue("api.marketing.baseurl")
+					+ MarketingConstants.PIC_MARKETING + taskVO.getStitchImagePath();
+		}
+		return new ServiceResponseBO(stitchImage);
 	}
 
 	@Transactional

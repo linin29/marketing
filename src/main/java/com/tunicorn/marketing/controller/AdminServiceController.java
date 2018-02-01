54716a8863cd1c89391d653c3f12db49d1b24b31
lining <lining@tunicorn.cn>
2018-02-01 18:12:35 +0800

    delete unuse import

--- src/main/java/com/tunicorn/marketing/controller/AdminServiceController.java
+++ src/main/java/com/tunicorn/marketing/controller/AdminServiceController.java
@@ -1,8 +1,6 @@
 package com.tunicorn.marketing.controller;
 
 import java.sql.Timestamp;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
--- src/main/java/com/tunicorn/marketing/job/ProjectReminderJob.java
+++ src/main/java/com/tunicorn/marketing/job/ProjectReminderJob.java
@@ -1,6 +1,5 @@
 package com.tunicorn.marketing.job;
 
-import java.text.ParseException;
 import java.util.Date;
 import java.util.List;
 
